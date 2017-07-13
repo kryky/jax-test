2ea0648d3f54b2bb235e2a12cf8598baf79a7b57
UI61LN <alexandrescucristian1995@yahoo.com>
2017-07-13 12:31:33 +0300

    Imported the form-data in the same class with the other one

--- src/main/java/com/mkyong/rest/HelloWorld.java
+++ src/main/java/com/mkyong/rest/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.mkyong.rest;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
@@ -21,6 +23,19 @@ public class HelloWorld {
                 + info.getQueryParameters().getFirst("from") + " and your hobbies are: "
                 + info.getQueryParameters().get("hobbies");
 
+        return Response.status(200).entity(msg).build();
+    }
+
+    @POST
+    @Path("/Info")
+    public Response getPath(
+            @FormParam("name") String name,
+            @FormParam("age") int age,
+            @FormParam("country") String country,
+            @FormParam("hobby") String hobby) {
+
+        String msg = "Your name is " + name + " and you are " + age + ". You are from " + country + " and your hobby is "
+                + hobby + ".";
         return Response.status(200).entity(msg).build();
     }
 
