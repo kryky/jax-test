5b1d6f0ca96e9cec6aead5c4df3035202b01481f
UI61LN <alexandrescucristian1995@yahoo.com>
2017-07-13 15:36:57 +0300

    Added FileDownload class

---
+++ src/main/java/com/mkyong/rest/FileDownload.java
@@ -0,0 +1,27 @@
+package com.mkyong.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.io.File;
+
+/**
+ * Created by UI61LN on 7/13/2017.
+ */
+@Path("/Download")
+public class FileDownload {
+
+    private static final String FILE_PATH = "D:\\\\Cristi\\file.txt";
+
+    @GET
+    @Path("/Text")
+    public Response downloadTextFile() {
+
+        File file = new File(FILE_PATH);
+
+        Response.ResponseBuilder response = Response.ok((Object) file);
+        response.header("Content-Disposition", "attachment; filename=\"file.txt\"");
+        return response.build();
+
+    }
+}
