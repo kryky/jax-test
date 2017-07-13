8b27964d0303dc85533e0f5bee49e29726716aba
UI61LN <alexandrescucristian1995@yahoo.com>
2017-07-13 11:26:31 +0300

    Using Context UriInfo

--- src/main/java/com/mkyong/rest/HelloWorld.java
+++ src/main/java/com/mkyong/rest/HelloWorld.java
@@ -2,8 +2,9 @@ package com.mkyong.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * Created by UI61LN on 7/13/2017.
@@ -13,10 +14,12 @@ import javax.ws.rs.core.Response;
 public class HelloWorld {
 
     @GET
-    @Path("/{param}")
-    public Response getPath(@PathParam("param") String message) {
-        String msg = "Your first word: " + message;
-        //blablbala
+    @Path("/")
+    public Response getPath(@Context UriInfo info) {
+        String msg = "Your name is " + info.getQueryParameters().getFirst("name") + " and you are "
+                + info.getQueryParameters().getFirst("age") + " old." + "You are from "
+                + info.getQueryParameters().getFirst("from") + " and your hobbies are: "
+                + info.getQueryParameters().get("hobbies");
 
         return Response.status(200).entity(msg).build();
     }
